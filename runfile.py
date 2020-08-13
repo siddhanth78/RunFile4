ef01bc4e2f871d58b7c99517724bdddbbca78b68
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-08-13 13:17:30 +0530

    Major changes, added delpath command

--- runfile.py
+++ runfile.py
@@ -1,8 +1,10 @@
 import os
 
+origin = os.getcwd()
 path = os.getcwd()
 print("RunFile")
-print("[Path]:{}\n".format(path))
+print("[OriginalPath]:{}\n".format(path))
+func=""
 
 while True:
     user = input("[Command]:")
@@ -14,6 +16,41 @@ while True:
         quit()
     else:
         pass
+
+    if user=='delpath':
+        if path==origin:
+           print("[InvalidPathOperation]:Cannot delete from original path.")
+           continue
+        count=0
+        lipath = path.split("\\")
+        lipath.pop(-1)
+        path=""
+        for l in lipath:
+            count+=1
+            if count==len(lipath):
+                path=path+l
+                break
+            path = path+l+"\\"
+        fucount=0
+        if "." not in func:
+            func=""
+        else:
+            lifu = func.split(".")
+            lifu.pop(-1)
+            func=""
+            for fu in lifu:
+                fucount+=1
+                if fucount==len(lifu):
+                    func=func+fu
+                    break
+                func=func+fu+"."
+        func=func.strip()
+        print("[NewPath]:{}\n".format(path))
+        continue
+
+    if "-" not in user:
+        print("[Error]:Command and file name must be seperated by '-'.")
+        continue
         
     try:
         li = user.split('-')
@@ -27,19 +64,37 @@ while True:
     else:
         pass
 
+    if comm=='addpath':
+        if "." in bfile:
+            print("[InvalidPathOperation]:Cannot add file to path.")
+            continue
+        if os.path.exists(path+"\\"+bfile)==False:
+            print("[InvalidPathOperation]:Such directory doesn't exist.")
+            continue
+        path = path+"\\"+bfile
+        if func.strip()=="":
+            func=bfile
+        else:
+            func = func+"."+bfile
+        func=func.strip()
+        print("[NewPath]:{}\n".format(path))
+        continue
+
     try:
         lif = bfile.split('.')
         file = lif[0]
         file = file.strip()
         ext = lif[1]
         ext = ext.strip()
+        file = file.replace("/",".")
+        file = file.replace("\\",".")
     except:
         print("[Error]:An error occured. Check your input and try again.")
         continue
     else:
         pass
 
-    if comm not in ['run','runfunc','funclist','findfunc','content']:
+    if comm not in ['runfile','runfunc','funclist','findfunc','content']:
         print("[InvalidCommand]:Command '{}' doesn't exist.".format(comm))
         continue
     elif comm in ['runfunc','funclist','findfunc'] and ext!='py':
@@ -48,39 +103,44 @@ while True:
     else:
         pass
 
-    if os.path.exists(bfile)==False:
+    if os.path.exists(path+"\\"+bfile)==False:
         print("[FileNotFound]:File '{}' doesn't exist.".format(bfile))
         continue
 
     if ext=='py':
-        func=""
         if comm=='content':
-            filee = open(bfile,'r')
+            filee = open(path+"\\"+bfile,'r')
             data = filee.read()
             print("\n{}\n".format(data))
-        elif comm=='run':
+        elif comm=='runfile':
             print("[Start]:{}".format(bfile))
-            os.system(bfile)
+            os.system(path+"\\"+bfile)
             print("[Stop]:{}".format(bfile))
         elif comm=='runfunc':
             arg = input("[Function]:")
             arg = arg.strip()
             if arg=='':
-                func=bfile
+                pass
             elif '(' not in arg or ')' not in arg:
                 print("[Error]:Function not completely defined.")
                 continue
             else:
-                func = file+'.'+arg
+                if func.strip()=="":
+                    runfunc = file+'.'+arg
+                else:
+                    runfunc = func+'.'+file+'.'+arg
             print("[Start]:{}".format(bfile))
             
             if arg=='':
-                os.system(func)
+                os.system(path+"\\"+bfile)
             else:
                 try:
-                    print("[Execute]:{}".format(func))
-                    exec("import {}".format(file))
-                    exec(func)
+                    print("[Execute]:{}".format(runfunc))
+                    if func=="":
+                        exec("import {}".format(file))
+                    else:
+                        exec("import {}".format(func+'.'+file))
+                    exec(runfunc)
                 except:
                     print("[Error]:An error occured. Check your input and try again.")
                     continue
@@ -89,7 +149,7 @@ while True:
             print("[Stop]:{}".format(bfile))
         elif comm=='funclist':
             print()
-            filee = open(bfile,'r')
+            filee = open(path+"\\"+bfile,'r')
             data = filee.readlines()
             for lines in data:
                 funcname=""
@@ -109,7 +169,7 @@ while True:
         elif comm=='findfunc':
             funct = input("[SearchFunction]:")
             print()
-            filee = open(bfile,'r')
+            filee = open(path+"\\"+bfile,'r')
             data = filee.readlines()
             for lines in data:
                 funcname=""
@@ -137,12 +197,12 @@ while True:
 
     else:
         if comm=='content':
-            filee = open(bfile,'r')
+            filee = open(path+"\\"+bfile,'r')
             data = filee.read()
             print("\n{}\n".format(data))
-        elif comm == 'run':
+        elif comm == 'runfile':
             print("[Start]:{}".format(bfile))
-            os.system(bfile)
+            os.system(path+"\\"+bfile)
             print("[Stop]:{}".format(bfile))
             
             
