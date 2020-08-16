b6ccdf175664785c49648335e221a093a0b0a0d8
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-08-16 17:30:03 +0530

    v3.4.2

--- RunFile3.py
+++ RunFile3.py
@@ -149,9 +149,9 @@ while True:
         print()
         for root, dirs, files in os.walk(path, topdown=False):
             for name in files:
-                print(os.path.join(root, name))
+                print("[File]:"+os.path.join(root, name))
             for name in dirs:
-                print(os.path.join(root, name))
+                print("[Directory]:"+os.path.join(root, name))
         print()
         continue
 
